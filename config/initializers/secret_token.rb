7d84ab9c00f0a9c98242128afa4a84ae267a1918
rambutan <rambutan@rambutan-HP-EliteBook-8460p.(none)>
2013-02-16 17:21:48 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opencv::Application.config.secret_token = '939da93a90f0f66f6e043528232d1b57ae99735c60a2b2f6ad381d760f4d74c1ebffc7e0e7586b4c1777b1ebcdf8bf4b98507fc3276a3e8531b2185824468020'
