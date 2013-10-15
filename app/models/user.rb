e9eb18933bb0cec50b5b8a3485b79855b3f5fcc4
Jakub Nieznalski <knusul@gmail.com>
2013-10-15 22:10:33 +0200

    set card email to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :create_card
   has_one :card, :dependent => :destroy
   has_many :experiences, :dependent => :destroy 
   has_many :educations, :dependent => :destroy
   has_many :skills, :dependent => :destroy
   has_many :languages, :dependent => :destroy
   has_many :hobbies, :dependent => :destroy
+  after_create :create_card
+
+  private
+  def create_card
+    self.card = Card.create :email => self.email
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,14 @@ describe User do
     @user = FactoryGirl.create :user
   end
 
+  context "create" do
+    it "make card together with user"
+    it "initialize card with same email" do
+      user = User.create :email => "email@example.com", :password => "foobar2baz"
+      user.card.email.should == 'email@example.com'
+    end
+  end
+
   context "destroy" do
     it "destroys dependent card" do
       @user.card = FactoryGirl.create(:card)
@@ -16,7 +24,7 @@ describe User do
   end
 
     [:experience, :skill, :hobby, :education].each do |attr|
-    it "destroys dependent experiences" do
+    it "destroys dependent #{attr}" do
       @user.send(ActiveSupport::Inflector.pluralize(attr)) << FactoryGirl.create(attr)
       expect do
         @user.destroy
