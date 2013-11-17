ed7414861d2f61aadfe6c06465b177624abc3214
Jakub Nieznalski <knusul@gmail.com>
2013-11-17 13:13:47 +0100

    Change errbit initializer password to api_key

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = Settings.airbreak.try(:password)
+  config.api_key = Settings.airbreak.try(:api_key)
   config.host    = 'err.open.cv'
   config.port    = 80
   config.secure  = config.port == 443
