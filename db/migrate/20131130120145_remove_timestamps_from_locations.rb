e378d2a89a840240cfb9d540b440f92bcfdbdc3c
Jakub Nieznalski <knusul@gmail.com>
2013-11-30 13:03:32 +0100

    remove timestamps from locations

---
+++ db/migrate/20131130120145_remove_timestamps_from_locations.rb
@@ -0,0 +1,6 @@
+class RemoveTimestampsFromLocations < ActiveRecord::Migration
+  def change
+    remove_column :locations, :created_at
+    remove_column :locations, :updated_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129205349) do
+ActiveRecord::Schema.define(version: 20131130120145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,14 +77,12 @@ ActiveRecord::Schema.define(version: 20131129205349) do
   end
 
   create_table "locations", force: true do |t|
-    t.string   "city"
-    t.string   "country"
-    t.string   "longitude"
-    t.string   "latitude"
-    t.integer  "visit_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "ip"
+    t.string  "city"
+    t.string  "country"
+    t.string  "longitude"
+    t.string  "latitude"
+    t.integer "visit_id"
+    t.string  "ip"
   end
 
   create_table "skills", force: true do |t|
